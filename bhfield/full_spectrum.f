a0126b53565368ee4a30f1d36576a5dd7d21aa36
Vincent Wheeler <vincent.m.wheeler@gmail.com>
2016-07-22 12:58:25 +1000

    progress

--- bhfield/full_spectrum.f
+++ bhfield/full_spectrum.f
@@ -25,6 +25,11 @@
       CHARACTER*80 DIRNAM,FILNAM(3),FNAME(3),IFNAME,IFILNAM
       CHARACTER*50 FNLOGF
       
+      integer npar
+      real*8 params(1)
+      external diffIsolPabs, bbfunc
+      
+      
 #ifdef CHECK_UNDERFLOW
       REAL*8 RDMIN
       CHARACTER*80 RDSTR
@@ -43,7 +48,7 @@
 C     !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 C     Declare all relevant physical constants
 C     !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-      WAVEL = 2.9D0
+      WAVEL = 0.28D0
       RADCOR = 0.03D0
       RADCOT = 0.06D0
       PI=ACOS(-1.0D0)
@@ -78,6 +83,7 @@ C      FILNAM(3)='Ag_palik.nk'
      1          FILNAM(I)(1:MAX(INDEX(FILNAM(I),' ')-1,1))
   901 CONTINUE
       
+      PRINT *,"initializing optical data"
       CALL OPTCON(1,WAVEL,FNAME,WLFAC, 
      1             REFMED,REFRE1,REFIM1,REFRE2,REFIM2)
       PRINT *, "Wavelength is: ", WAVEL
@@ -87,6 +93,13 @@ C      FILNAM(3)='Ag_palik.nk'
       PRINT *, "got: ",REFRE1," + i*",REFIM1," for ",WAVEL," micron"
       PRINT *, "using this nk data for shell medium: ", FNAME(3)
       PRINT *, "got: ",REFRE2," + i*",REFIM2," for ",WAVEL," micron"
+      
+      
+      PRINT *, "initializing solar intensity data"
+      IFILNAM="ASTMG173.csv"
+      IFNAME=DIRNAM(1:MAX(INDEX(DIRNAM,' ')-1,1))//
+     1          IFILNAM(1:MAX(INDEX(IFILNAM,' ')-1,1))
+      CALL SOLARINTENSITY(1,WAVEL,IFNAME,INTENSITY,2)
       
 C     get relative refractive indices
       RFREL1=DCMPLX(REFRE1,REFIM1)/REFMED
@@ -147,11 +160,8 @@ C
       
       PRINT *, "Here it is: ", ANS
       
-      IFILNAM="ASTMG173.csv"
-      IFNAME=DIRNAM(1:MAX(INDEX(DIRNAM,' ')-1,1))//
-     1          IFILNAM(1:MAX(INDEX(IFILNAM,' ')-1,1))
-      CALL SOLARINTENSITY(1,WAVEL,IFNAME,INTENSITY,2)
       
+      CALL SOLARINTENSITY(0,WAVEL,IFNAME,INTENSITY,2)
       PRINT *, "And here is the intensity at ", WAVEL, "um"
       PRINT *, "  " , INTENSITY
       
@@ -166,17 +176,408 @@ C
       
       
       
-      CALL IntegrateBB(ANS,TEMP,1)
+      CALL IntegrateBB(ANS,TEMP,4)
       PRINT *,"integral over whole spectrum should give sigT^4"
       PRINT *, "MINE: ", ANS
       PRINT *, "Should be: ", 5.670E-8*TEMP**4.0D0
       
+      
+      npar = 1
+      params(1) = TEMP
+      
+      
+      CALL IntegrateGeneric(ANS,diffIsolPabs,params,npar,1)
+      PRINT *, ""
+      PRINT *, "How did I do?"
+      PRINT *, ANS
+      
+      CALL IntegrateGeneric(ANS,bbfunc,params,npar,4)
+      PRINT *, ""
+      PRINT *, "This thingy should should give me the same result as "
+      PRINT *, "IntegrateBB: ", ANS
+      
       stop
       end
       
+
+  
+  
+C ********************************************************************
+C integral over blackbody at given temp using 4 choices for integration scheme
+C ********************************************************************
+C   choice = 1 --> Vegas
+C   choice = 2 --> Suave  
+C   choice = 3 --> Divonne  
+C   choice = 4 --> Cuhre
+  
+      SUBROUTINE IntegrateGeneric(ANS,extfunc,userdata,npar,choice)
+
+      implicit none
+
+      integer ndim, ncomp, nvec, last, seed, mineval, maxeval
+      cubareal epsrel, epsabs
+      
+      parameter (ndim = 3)
+      parameter (ncomp = 1)
+      parameter (nvec = 1)
+      parameter (epsrel = 1D-5)
+      parameter (epsabs = 1D-12)
+      parameter (last = 4)
+      parameter (seed = 0)
+      parameter (mineval = 0)
+      parameter (maxeval = 100000)
+
+      integer nstart, nincrease, nbatch, gridno,choice
+      integer*8 spin
+      character*(*) statefile
+      parameter (nstart = 1000)
+      parameter (nincrease = 500)
+      parameter (nbatch = 1000)
+      parameter (gridno = 0)
+      parameter (statefile = "")
+      parameter (spin = -1)
+
+      integer nnew, nmin
+      cubareal flatness
+      parameter (nnew = 1000)
+      parameter (nmin = 2)
+      parameter (flatness = 25D0)
+
+      integer key1, key2, key3, maxpass
+      cubareal border, maxchisq, mindeviation
+      integer ngiven, ldxgiven, nextra
+      parameter (key1 = 47)
+      parameter (key2 = 1)
+      parameter (key3 = 1)
+      parameter (maxpass = 5)
+      parameter (border = 0D0)
+      parameter (maxchisq = 10D0)
+      parameter (mindeviation = .25D0)
+      parameter (ngiven = 0)
+      parameter (ldxgiven = ndim)
+      parameter (nextra = 0)
+
+      integer key
+      parameter (key = 0)
+      
+      integer npar
+      real*8 userdata(npar)
+      
+      REAL*8 ANS
+
+      external extfunc
+
+      cubareal integral(ncomp), error(ncomp), prob(ncomp)
+      integer verbose, neval, fail, nregions
+      character*16 env
+
+      integer c
+     
+      print *, "this should be 1: ", npar
+      print *, "this should be TEMP: ", userdata(1)
+     
+     
+      call getenv("CUBAVERBOSE", env)
+      verbose = 2
+      read(env, *, iostat=fail, end=999, err=999) verbose
+  999 continue
+
+
+      if (choice == 1) then
+        print *, "----- using Vegas -----"
+        call Vegas(ndim, ncomp, extfunc, userdata, nvec, 
+     &    epsrel, epsabs, verbose, seed, 
+     &    mineval, maxeval, nstart, nincrease, nbatch, 
+     &    gridno, statefile, spin, 
+     &    neval, fail, integral, error, prob)
+
+        print *, "neval    =", neval
+        print *, "fail     =", fail
+        print '(F20.12," +- ",F20.12,"   p = ",F8.3)', 
+     &     (integral(c), error(c), prob(c), c = 1, ncomp)
+      
+      else if (choice == 2) then
+        print *, "----- using Suave -----"
+        call suave(ndim, ncomp, extfunc, userdata, nvec,
+     &    epsrel, epsabs, verbose + last, seed,
+     &    mineval, maxeval, nnew, nmin, flatness,
+     &    statefile, spin,
+     &    nregions, neval, fail, integral, error, prob)
+
+        print *, "nregions =", nregions
+        print *, "neval    =", neval
+        print *, "fail     =", fail
+        print '(F20.12," +- ",F20.12,"   p = ",F8.3)',
+     &    (integral(c), error(c), prob(c), c = 1, ncomp)
+      
+      else if (choice == 3) then
+        print *, "----- using Divonne -----"
+        call divonne(ndim, ncomp, extfunc, userdata, nvec,
+     &    epsrel, epsabs, verbose, seed,
+     &    mineval, maxeval, key1, key2, key3, maxpass,
+     &    border, maxchisq, mindeviation,
+     &    ngiven, ldxgiven, 0, nextra, 0,
+     &    statefile, spin,
+     &    nregions, neval, fail, integral, error, prob)
+
+        print *, "nregions =", nregions
+        print *, "neval    =", neval
+        print *, "fail     =", fail
+        print '(F20.12," +- ",F20.12,"   p = ",F8.3)',
+     &    (integral(c), error(c), prob(c), c = 1, ncomp)
+      
+      else if (choice == 4) then
+        print *, "----- using Cuhre -----"
+        call cuhre(ndim, ncomp, extfunc, userdata, nvec,
+     &    epsrel, epsabs, verbose + last,
+     &    mineval, maxeval, key,
+     &    statefile, spin,
+     &    nregions, neval, fail, integral, error, prob)
+
+        print *, "nregions =", nregions
+        print *, "neval    =", neval
+        print *, "fail     =", fail
+        print '(F20.12," +- ",F20.12,"   p = ",F8.3)',
+     &    (integral(c), error(c), prob(c), c = 1, ncomp)
+      
+      end if
       
       
       
+      ANS = integral(1)
+
+      RETURN
+      END 
+        
+C ********************************************************************
+C This is the integrand I_sol*p_abs to be integrated over angle and wavelength
+C ********************************************************************
+
+        integer function diffIsolPabs(ndim, xx, ncomp, ff)
+        implicit none
+        integer ndim, ncomp
+        cubareal xx(*),ff(*),th,ph,lam
+        real*8 pi
+        
+#define x xx(1)
+#define y xx(2)
+#define z xx(3)
+#define f ff(1)
+      
+      
+
+        pi=acos(-1.0D0)
+        
+        th = x*pi
+        ph = y*2.0D0*pi
+        
+        lam = 0.28D0 + (4.0D0-0.28D0)*z !shift to integral over solar spectrum
+
+        
+        
+        ! note first chunk is necessary to change integration interval
+        f = (2.0D0*pi*pi* (4.0D0-0.28D0) ) * th*ph*lam
+
+        diffIsolPabs = 0
+        end
+
+        
+        
+C ********************************************************************
+C This thing should return the ASTM solar spectrum 
+C ********************************************************************
+C   WHICH = 1 --> Extraterrestrial radiation
+C   WHICH = 2 --> Global tilt
+C   WHICH = 3 --> Direct + circumsolar
+
+      SUBROUTINE SOLARINTENSITY(INIT,WAVEL,FNAME,INTENSITY,WHICH)
+C
+C get n,k values at specified wavelength
+C
+      IMPLICIT NONE
+      INTEGER NDMAX, WHICH
+      PARAMETER (NDMAX=5000)
+      INTEGER INIT,J
+      REAL*8 WAVEL
+      REAL*8 DATWL,ETR,GTILT,DIRCIRC,RAT,INTENSITY
+      REAL*8 DOPT(1,4,NDMAX),WLFAC
+      INTEGER ND
+      CHARACTER*80 FNAME
+      CHARACTER*255 BUF
+      SAVE DOPT,ND
+C
+      IF(INIT.EQ.1) THEN
+        OPEN(1,FILE=FNAME,STATUS='OLD')
+        J=0
+  562   CONTINUE
+        READ(1,'(A)',END=561) BUF
+        IF(BUF(1:1).EQ.'#'.OR.BUF(1:1).EQ.';') GO TO 562
+        IF(INDEX(BUF,'.').EQ.0) GO TO 562
+        READ(BUF,*,ERR=562) DATWL,ETR,GTILT,DIRCIRC
+        IF(DATWL.EQ.0.0D0) GO TO 562
+        J=J+1
+        IF(J.GE.NDMAX) THEN
+         WRITE(*,*) 'ndata > max for ',FNAME
+         STOP
+        END IF
+        WLFAC = 1.0D-3 !hardcoded!
+        DOPT(1,1,J)=DATWL*WLFAC
+        DOPT(1,2,J)=ETR
+        DOPT(1,3,J)=GTILT
+        DOPT(1,4,J)=DIRCIRC
+        GO TO 562
+  561   CONTINUE
+        CLOSE(1)
+        ND=J
+  570   FORMAT('Reading n,k(',I1,'): ndata = ',I4,' wavelength = ',
+     *  E13.6,' - ',E13.6,' file = [',A,']')
+        WRITE(*,570) 1,ND,DOPT(1,1,1),DOPT(1,1,ND),TRIM(FNAME)
+        WRITE(51,570) 1,ND,DOPT(1,1,1),DOPT(1,1,ND),TRIM(FNAME)
+
+      END IF
+C
+       IF(WAVEL.LT.DOPT(1,1,1).OR.WAVEL.GT.DOPT(1,1,ND)) THEN
+        WRITE(*,*) 'wavelength out of range! ',WAVEL,' SI'
+        STOP
+       END IF
+       DO 510 J=2,ND
+C wavelength is in the increasing order
+        IF(WAVEL.LE.DOPT(1,1,J)) GO TO 511
+  510  CONTINUE
+  511  CONTINUE
+C simple interpolation
+       RAT=(WAVEL-DOPT(1,1,J-1))/(DOPT(1,1,J)-DOPT(1,1,J-1))
+       
+       INTENSITY=DOPT(1,WHICH+1,J-1)+(DOPT(1,WHICH+1,J)
+     &  -DOPT(1,WHICH+1,J-1))*RAT
+
+C
+C
+      RETURN
+      END
+      
+      
+C ***********************************************************************
+C Function to calculate blackbody emissive power at a given temperature
+C  as a function of wavelength (in micron)
+C ***********************************************************************
+      
+      SUBROUTINE BBEPOW(lambda, t, eb) 
+
+      implicit none
+
+
+      real*8 lambda, t, lambda_m
+      real*8 eb
+      REAL*8 c_first_radiation
+      REAL*8 c_second_radiation
+      ! -- Local declarations --
+
+      real*8 beta
+      
+      print *, "did I make it?"
+      
+      lambda_m = lambda*1.0D-6 !converstion from micron to meter
+      !lambda_m = lambda!converstion from micron to meter
+      
+      c_first_radiation = 3.74177118e-16
+      c_second_radiation = 14387.75225e-06
+      beta  = c_second_radiation / (lambda_m * t)
+      eb = c_first_radiation  / (lambda_m ** 5 * (exp(beta) - 1.0D0))
+
+      
+
+      return
+      end
+      
+      
+      SUBROUTINE BBEPOWM(lambda, t, eb) 
+
+      implicit none
+
+
+      real*8 lambda, t, lambda_m
+      real*8 eb
+      REAL*8 c_first_radiation
+      REAL*8 c_second_radiation
+      ! -- Local declarations --
+
+      real*8 beta
+      
+      !lambda_m = lambda*1.0D-6 !converstion from micron to meter
+      lambda_m = lambda*1.0D0!NO converstion from micron to meter
+      
+      c_first_radiation = 3.74177118e-16
+      c_second_radiation = 14387.75225e-06
+      beta  = c_second_radiation / (lambda_m * t)
+      eb = c_first_radiation  / (lambda_m ** 5 * (exp(beta) - 1.0D0))
+
+      
+
+      return
+      end
+      
+C ***********************************************************************
+C Function to calculate temperature derivative of the 
+C  blackbody emissive power at a given temperature
+C  as a function of wavelength (in micron)
+C ***********************************************************************
+      
+      SUBROUTINE BBEPOWDT(lambda, t, ebdt) 
+
+      implicit none
+
+
+      real*8 lambda, t, lambda_m
+      real*8 ebdt
+      REAL*8 c1
+      REAL*8 c2
+      ! -- Local declarations --
+
+      
+      lambda_m = lambda*1.0D-6 !converstion from micron to meter
+      
+      c1 = 3.74177118e-16
+      c2 = 14387.75225e-06
+
+      ebdt = (c1*c2*EXP(c2/(t*lambda_m)))
+      ebdt = ebdt/((-1.0D0+EXP(c2/(t*lambda_m)))**2*t**2*lambda_m**6)
+    
+      return
+      end
+      
+      SUBROUTINE BBEPOWDTM(lambda, t, ebdt) 
+
+      implicit none
+
+
+      real*8 lambda, t, lambda_m
+      real*8 ebdt
+      REAL*8 c1
+      REAL*8 c2
+      ! -- Local declarations --
+
+      
+      lambda_m = lambda*1.0D0 !NO converstion from micron to meter
+      
+      c1 = 3.74177118e-16
+      c2 = 14387.75225e-06
+
+      ebdt = (c1*c2*EXP(c2/(t*lambda_m)))
+      ebdt = ebdt/((-1.0D0+EXP(c2/(t*lambda_m)))**2*t**2*lambda_m**6)
+    
+      return
+      end
+
+      
+C ********************************************************************
+C integral over blackbody at given temp using 4 choices for integration scheme
+C ********************************************************************
+C   choice = 1 --> Vegas
+C   choice = 2 --> Suave  
+C   choice = 3 --> Divonne  
+C   choice = 4 --> Cuhre
+
       SUBROUTINE IntegrateBB(ANS,TEMP,choice)
 
       implicit none
@@ -187,12 +588,12 @@ C
       parameter (ndim = 3)
       parameter (ncomp = 1)
       parameter (nvec = 1)
-      parameter (epsrel = 1D-4)
+      parameter (epsrel = 1D-5)
       parameter (epsabs = 1D-12)
       parameter (last = 4)
       parameter (seed = 0)
       parameter (mineval = 0)
-      parameter (maxeval = 50000)
+      parameter (maxeval = 100000)
 
       integer nstart, nincrease, nbatch, gridno,choice
       integer*8 spin
@@ -377,187 +778,4 @@ C        f = SIN(x)*EXP(-x)
 
         sinexp = 0
         end
-        
-C ********************************************************************
-C This thing should return the ASTM solar spectrum 
-C ********************************************************************
-C   WHICH = 1 --> Extraterrestrial radiation
-C   WHICH = 2 --> Global tilt
-C   WHICH = 3 --> Direct + circumsolar
-
-      SUBROUTINE SOLARINTENSITY(INIT,WAVEL,FNAME,INTENSITY,WHICH)
-C
-C get n,k values at specified wavelength
-C
-      IMPLICIT NONE
-      INTEGER NDMAX, WHICH
-      PARAMETER (NDMAX=5000)
-      INTEGER INIT,J
-      REAL*8 WAVEL
-      REAL*8 DATWL,ETR,GTILT,DIRCIRC,RAT,INTENSITY
-      REAL*8 DOPT(1,4,NDMAX),WLFAC
-      INTEGER ND
-      CHARACTER*80 FNAME
-      CHARACTER*255 BUF
-      SAVE DOPT,ND
-C
-      IF(INIT.EQ.1) THEN
-        OPEN(1,FILE=FNAME,STATUS='OLD')
-        J=0
-  562   CONTINUE
-        READ(1,'(A)',END=561) BUF
-        IF(BUF(1:1).EQ.'#'.OR.BUF(1:1).EQ.';') GO TO 562
-        IF(INDEX(BUF,'.').EQ.0) GO TO 562
-        READ(BUF,*,ERR=562) DATWL,ETR,GTILT,DIRCIRC
-        IF(DATWL.EQ.0.0D0) GO TO 562
-        J=J+1
-        IF(J.GE.NDMAX) THEN
-         WRITE(*,*) 'ndata > max for ',FNAME
-         STOP
-        END IF
-        WLFAC = 1.0D-3 !hardcoded!
-        DOPT(1,1,J)=DATWL*WLFAC
-        DOPT(1,2,J)=ETR
-        DOPT(1,3,J)=GTILT
-        DOPT(1,4,J)=DIRCIRC
-        GO TO 562
-  561   CONTINUE
-        CLOSE(1)
-        ND=J
-  570   FORMAT('Reading n,k(',I1,'): ndata = ',I4,' wavelength = ',
-     *  E13.6,' - ',E13.6,' file = [',A,']')
-        WRITE(*,570) 1,ND,DOPT(1,1,1),DOPT(1,1,ND),TRIM(FNAME)
-        WRITE(51,570) 1,ND,DOPT(1,1,1),DOPT(1,1,ND),TRIM(FNAME)
-
-      END IF
-C
-       IF(WAVEL.LT.DOPT(1,1,1).OR.WAVEL.GT.DOPT(1,1,ND)) THEN
-        WRITE(*,*) 'wavelength out of range! ',WAVEL,' SI'
-        STOP
-       END IF
-       DO 510 J=2,ND
-C wavelength is in the increasing order
-        IF(WAVEL.LE.DOPT(1,1,J)) GO TO 511
-  510  CONTINUE
-  511  CONTINUE
-C simple interpolation
-       RAT=(WAVEL-DOPT(1,1,J-1))/(DOPT(1,1,J)-DOPT(1,1,J-1))
-       
-       INTENSITY=DOPT(1,WHICH+1,J-1)+(DOPT(1,WHICH+1,J)
-     &  -DOPT(1,WHICH+1,J-1))*RAT
-
-C
-C
-      RETURN
-      END
-      
-      
-C ***********************************************************************
-C Function to calculate blackbody emissive power at a given temperature
-C  as a function of wavelength (in micron)
-C ***********************************************************************
-      
-      SUBROUTINE BBEPOW(lambda, t, eb) 
-
-      implicit none
-
-
-      real*8 lambda, t, lambda_m
-      real*8 eb
-      REAL*8 c_first_radiation
-      REAL*8 c_second_radiation
-      ! -- Local declarations --
-
-      real*8 beta
-      
-      lambda_m = lambda*1.0D-6 !converstion from micron to meter
-      !lambda_m = lambda!converstion from micron to meter
-      
-      c_first_radiation = 3.74177118e-16
-      c_second_radiation = 14387.75225e-06
-      beta  = c_second_radiation / (lambda_m * t)
-      eb = c_first_radiation  / (lambda_m ** 5 * (exp(beta) - 1.0D0))
-
-      
-
-      return
-      end
-      
-      
-      SUBROUTINE BBEPOWM(lambda, t, eb) 
-
-      implicit none
-
-
-      real*8 lambda, t, lambda_m
-      real*8 eb
-      REAL*8 c_first_radiation
-      REAL*8 c_second_radiation
-      ! -- Local declarations --
-
-      real*8 beta
-      
-      !lambda_m = lambda*1.0D-6 !converstion from micron to meter
-      lambda_m = lambda*1.0D0!NO converstion from micron to meter
-      
-      c_first_radiation = 3.74177118e-16
-      c_second_radiation = 14387.75225e-06
-      beta  = c_second_radiation / (lambda_m * t)
-      eb = c_first_radiation  / (lambda_m ** 5 * (exp(beta) - 1.0D0))
-
-      
-
-      return
-      end
-      
-C ***********************************************************************
-C Function to calculate temperature derivative of the 
-C  blackbody emissive power at a given temperature
-C  as a function of wavelength (in micron)
-C ***********************************************************************
-      
-      SUBROUTINE BBEPOWDT(lambda, t, ebdt) 
-
-      implicit none
-
-
-      real*8 lambda, t, lambda_m
-      real*8 ebdt
-      REAL*8 c1
-      REAL*8 c2
-      ! -- Local declarations --
-
-      
-      lambda_m = lambda*1.0D-6 !converstion from micron to meter
-      
-      c1 = 3.74177118e-16
-      c2 = 14387.75225e-06
-
-      ebdt = (c1*c2*EXP(c2/(t*lambda_m)))
-      ebdt = ebdt/((-1.0D0+EXP(c2/(t*lambda_m)))**2*t**2*lambda_m**6)
-    
-      return
-      end
-      
-      SUBROUTINE BBEPOWDTM(lambda, t, ebdt) 
-
-      implicit none
-
-
-      real*8 lambda, t, lambda_m
-      real*8 ebdt
-      REAL*8 c1
-      REAL*8 c2
-      ! -- Local declarations --
-
-      
-      lambda_m = lambda*1.0D0 !NO converstion from micron to meter
-      
-      c1 = 3.74177118e-16
-      c2 = 14387.75225e-06
-
-      ebdt = (c1*c2*EXP(c2/(t*lambda_m)))
-      ebdt = ebdt/((-1.0D0+EXP(c2/(t*lambda_m)))**2*t**2*lambda_m**6)
-    
-      return
-      end
+  
