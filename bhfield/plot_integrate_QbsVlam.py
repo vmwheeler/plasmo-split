da88357c9e3e686a69692d1513e18e08e9ff0c12
Vincent Wheeler <vincent.m.wheeler@gmail.com>
2016-07-26 18:46:11 +1000

    this guy integrates Qabs over lambda to verify full_spectrum.f result

---
+++ bhfield/plot_integrate_QbsVlam.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import numpy as np
+from scipy.integrate import simps
+import matplotlib.pyplot as plt
+
+f = open("./QabsVlam.dat")
+
+data = np.loadtxt("./QabsVlam.dat",skiprows=3)
+
+#check that this integrates to proper absorption cross section
+lams = data[:,0] # in micrometers
+Qabss = data[:,1]
+
+
+print lams
+
+integval = simps(Qabss,lams*1.0E-6)
+print "oh please: " + str(integval)
+
+plt.figure()
+plt.plot(lams*1.0E3,Qabss)
+plt.xlabel('wavelength [nm]')
+plt.ylabel('Q_abs [1]')
+plt.show()
+
